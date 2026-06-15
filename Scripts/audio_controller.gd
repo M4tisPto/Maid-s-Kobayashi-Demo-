25c4a97be433bcb06666fdd5ee1bbdf71f3d3365
M4tisPto <matis.pto.104250@gmail.com>
2026-06-15 12:58:07 -0400

    osaka!!

--- Scripts/audio_controller.gd
+++ Scripts/audio_controller.gd
@@ -2,21 +2,24 @@ extends Node
 
 @export var mute: bool = false
 
-var sounds := {}
 
-func _ready():
-	sounds = {
-		"jump": $Jump,
-		"punch": $Punch,
-		"music_menu": $Music_menu,
-		"music_lvl": $Music_lvl_test,
-		"test": $test_sound
-	}
+func play_music_test():
+	if not mute:
+		$Music/Music_lvl_test.play()
 
-func play_sound(sound_name: String):
-	if sounds.has(sound_name):
-		sounds[sound_name].play()
+func stop_music_test():
+	$Music/Music_lvl_test.stop()
 
-func stop_sound(sound_name: String):
-	if sounds.has(sound_name):
-		sounds[sound_name].stop()
+func play_menu_music():
+	if not mute:
+		$Music/Music_menu.play()
+
+func stop_menu_music():
+	$Music/Music_menu.stop()
+
+func play_jump():
+	if not mute:
+		$Sfx/Jump.play()
+
+func stop_play_jump():
+	$Sfx/Jump.stop()
--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends CharacterBody2D
 
+@export var max_hp: int = 100
+@onready var current_hp: int = max_hp
 @onready var movement_state_machine: Node = $movement_state_machine
 @onready var attack_state_machine: Node = $attack_state_machine
 @onready var collision_hitbox: CollisionShape2D = $Hitbox/collision_hitbox
