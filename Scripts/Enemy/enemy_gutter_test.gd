564b736ad4f5b78730f6287e8ae3427eed07f861
M4tisPto <matis.pto.104250@gmail.com>
2026-06-17 12:50:11 -0400

    enemy ai movement

--- Scripts/Enemy/enemy_gutter_test.gd
+++ Scripts/Enemy/enemy_gutter_test.gd
@@ -1,2 +1,30 @@
-class_name GutterEnemy
 extends CharacterBody2D
+
+@export var speed := 100
+@export var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction := 1
+var player: Player
+@onready var ground_check  = $GroundRay
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	velocity.x = direction * speed
+	
+	move_and_slide()
+	if is_on_wall() or !ground_check.is_colliding():
+		direction *= -1
+	
+	update_raycast_direction()
+
+
+
+
+func update_raycast_direction():
+	ground_check.target_position = Vector2(direction * 20, 20)
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body == player:
+		queue_free()
