6ee15d86331f9b69890eaa43673699e340661484
M4tisPto <matis.pto.104250@gmail.com>
2026-08-13 23:31:34 -0400

    idk

--- Scripts/state_machine/attack_state_machine/copy_arm.gd
+++ Scripts/state_machine/attack_state_machine/copy_arm.gd
@@ -1,23 +1,36 @@
 extends State
+
 @export var base_state: State
 @export var knuckleblaster_state: State
-var timer = 0
+
+var timer := 0.0
+var next_state: State = null
+
+
 func enter() -> void:
-	timer = 0.1
+	timer = 0.5
+	next_state = null
+
 	print("copy state entered")
-	
+
 	parent.collision_copy.set_deferred("disabled", false)
+
+
+func exit() -> void:
+	parent.collision_copy.set_deferred("disabled", true)
+
 
 func process_physics(delta: float) -> State:
 	timer -= delta
-	if parent.copy_area.get_overlapping_areas().is_empty():
-		parent.collision_copy.set_deferred("disabled", true)
+
+	if next_state:
+		return next_state
+	if timer <= 0:
 		return base_state
+
 	return null
 
-func _on_copy_area_area_entered(area: Area2D):
 
-	print("Collision detected with: ", area.name)
-	if area.name == "Hurtbox":
-		return knuckleblaster_state
-		
+func _on_copy_area_area_entered(area: Area2D) -> void:
+	if area.name == "CopyArea":
+		next_state = knuckleblaster_state
